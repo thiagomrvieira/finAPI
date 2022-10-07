8972a13797e4bfec2445128efd7146daa08e5ec2
Thiago Vieira <tmaciel@digital-works.com>
2022-10-07 12:43:59 +0100

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 
@@ -21,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     request.customer = customer;
 
     return next();
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit' ) {
+            return acc + operation.amount
+        }else {
+            return acc - operation.amount
+        }
+    }, 0);
+
+    return balance;
 }
 
 /**
@@ -75,6 +88,27 @@ app.post("/statement", verifyIfExistsAccountCPF, (request, response) => {
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
-})
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount, 
+        created_at: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send()
+});
 
 app.listen(3333);
