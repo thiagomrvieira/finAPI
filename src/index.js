6262ffc87c0831494f305e7cc4328ace7f9ff903
Thiago Vieira <tmaciel@digital-works.com>
2022-10-07 12:03:27 +0100

    Criando depósito na conta

--- src/index.js
+++ src/index.js
@@ -55,9 +55,26 @@ app.post("/account", (request, response) => {
 // Add middleware for next routes
 // app.use(verifyIfExistsAccountCPF);
 
-app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
     return response.json(customer.statement);
 });
+
+app.post("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount, 
+        created_at: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
 
 app.listen(3333);
